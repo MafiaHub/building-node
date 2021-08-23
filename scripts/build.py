c6520a8f0c0ee2116d5f7743df8d777d115cfd83
Segfault <5221072+Segfaultd@users.noreply.github.com>
2021-08-23 16:31:27 +0200

    Only build x86 on windows

--- scripts/build.py
+++ scripts/build.py
@@ -9,7 +9,7 @@ from . import config
 
 os.chdir('node-{}'.format(config.nodeVersion))
 
-configureArgvs = [ '--enable-static', '--without-node-options', '--dest-cpu=x86' ] + config.configFlags
+configureArgvs = [ '--enable-static', '--without-node-options' ] + config.configFlags
 
 if sys.platform == 'win32':
     env = os.environ.copy()
