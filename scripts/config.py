8e5a22d92c441175e7487391ce0b04a5b9af7ebb
Segfault <5221072+Segfaultd@users.noreply.github.com>
2021-10-25 12:26:01 +0200

    Actually works better like this

--- scripts/config.py
+++ scripts/config.py
@@ -4,5 +4,5 @@ import os
 
 nodeVersion = os.environ['NODE_TARGET_VERSION'] if 'NODE_TARGET_VERSION' in os.environ else 'v16.6.1'
 nodeTargetConfig = os.environ['NODE_TARGET_CONFIG'] if 'NODE_TARGET_CONFIG' in os.environ else 'Debug'
-configFlags = ['--without-intl']
-zipBasenameSuffix = '-nointl'
+configFlags = ['--with-intl=small-icu']
+zipBasenameSuffix = '-smallicu'
